46a0d33a8fb1f68a8d0a6f5e0ee9b37c762f5554
Your Name <you@example.com>
2025-05-29 01:39:24 +0900

    Bug fix and modified sample structures

--- hadd.sh
+++ hadd.sh
@@ -4,7 +4,7 @@ BASE_DIR="./output/250517"
 ERAS=("2016_postVFP" "2016_preVFP" "2017" "2018")
 
 for era in "${ERAS[@]}"; do
-  echo "Processing era: $era"
+  echo -e "\n========================= Processing era: $era ========================="
   
   for sample_dir in "$BASE_DIR/$era"/*; do
     if [ ! -d "$sample_dir" ]; then
@@ -12,7 +12,7 @@ for era in "${ERAS[@]}"; do
     fi
     
     sample=$(basename "$sample_dir")
-    echo -e "Processing sample: $sample\n"
+    echo -e "\n========================= Processing sample: $sample =========================\n"
     
     combined_dir="$sample_dir/combined"
     mkdir -p "$combined_dir"
@@ -39,12 +39,14 @@ done
 
 # Final combination
 for era in "${ERAS[@]}"; do
-  echo -e "\nProcessing final combination for era: $era\n"
+  echo -e "\n========================= Processing final combination for era: $era =========================\n"
   
   dy_files=""
   st_files=""
   tt_files=""
-  wz_files=""
+  ew_files=""
+  wjets_files=""
+  dytau_files=""
   data_files=""
   
   for sample_dir in "$BASE_DIR/$era"/*; do
@@ -61,7 +63,7 @@ for era in "${ERAS[@]}"; do
     # Data
     if [[ "$sample" == *"Run"* ]]; then
       data_files="$data_files $sample_dir/combined/*.root"
-    elif [[ "$sample" == DY* ]]; then
+    elif [[ "$sample" == DYJetsToMuMu* ]]; then
       # DY
       dy_files="$dy_files $sample_dir/combined/*.root"
     elif [[ "$sample" == ST* ]]; then
@@ -70,9 +72,15 @@ for era in "${ERAS[@]}"; do
     elif [[ "$sample" == TT* ]]; then
       # TT 
       tt_files="$tt_files $sample_dir/combined/*.root"
-    elif [[ "$sample" == WJets* || "$sample" == WZ* || "$sample" == ZZ* ]]; then
-      # WJets, WZ, ZZ 
-      wz_files="$wz_files $sample_dir/combined/*.root"
+    elif [[ "$sample" == WW* || "$sample" == WZ || "$sample" == ZZ ]]; then
+      # EW
+      ew_files="$ew_files $sample_dir/combined/*.root"
+    elif [[ "$sample" == WJets* ]]; then
+      # Wjets
+      wjets_files="$wjets_files $sample_dir/combined/*.root"
+    elif [[ "$sample" == DYJetsToTauTau* ]]; then
+      # DY_TauTau
+      dytau_files="$dytau_files $sample_dir/combined/*.root"
     fi
   done
   
@@ -81,31 +89,50 @@ for era in "${ERAS[@]}"; do
   
   if [ ! -z "$dy_files" ]; then
     hadd_command="hadd -f $final_dir/DY_$era.root $dy_files"
-    echo "  $hadd_command"
+    echo -e "========================= DY =========================\n"
+    echo "$hadd_command"
     eval "$hadd_command"
   fi
   
   if [ ! -z "$st_files" ]; then
     hadd_command="hadd -f $final_dir/ST_$era.root $st_files"
-    echo "  $hadd_command"
+    echo -e "\n========================= ST =========================\n"
+    echo "$hadd_command"
     eval "$hadd_command"
   fi
   
   if [ ! -z "$tt_files" ]; then
     hadd_command="hadd -f $final_dir/TT_$era.root $tt_files"
-    echo "  $hadd_command"
+    echo -e "\n========================= TT =========================\n"
+    echo "$hadd_command"
     eval "$hadd_command"
   fi
   
-  if [ ! -z "$wz_files" ]; then
-    hadd_command="hadd -f $final_dir/WZ_$era.root $wz_files"
-    echo "  $hadd_command"
+  if [ ! -z "$ew_files" ]; then
+    hadd_command="hadd -f $final_dir/EW_$era.root $ew_files"
+    echo -e "\n========================= EW =========================\n"
+    echo "$hadd_command"
+    eval "$hadd_command"
+  fi
+
+  if [ ! -z "$wjets_files" ]; then
+    hadd_command="hadd -f $final_dir/WJets_$era.root $wjets_files"
+    echo -e "\n======================= WJets =========================\n"
+    echo "$hadd_command"
+    eval "$hadd_command"
+  fi
+
+  if [ ! -z "$dytau_files" ]; then
+    hadd_command="hadd -f $final_dir/DY_TauTau_$era.root $dytau_files"
+    echo -e "\n===================== DY_TauTau ======================\n"
+    echo "$hadd_command"
     eval "$hadd_command"
   fi
   
   if [ ! -z "$data_files" ]; then
     hadd_command="hadd -f $final_dir/Data_$era.root $data_files"
-    echo "  $hadd_command"
+    echo -e "\n======================= Data =========================\n"
+    echo "$hadd_command"
     eval "$hadd_command"
   fi
 done
